2ea58d9f4b6eee388af98cf7b4ed395ef2fbc2ed
Juan Estrada <juanestrada@Juans-MBP.Home>
2015-10-14 08:09:40 -0500

    first commit

---
+++ js/app-browserify.js
@@ -0,0 +1,14 @@
+// es5 polyfills, powered by es5-shim
+require("es5-shim")
+
+// es6 polyfills, powered by babel
+require("babel/polyfill")
+
+var Promise = require('es6-promise').Promise
+
+import $ from 'jquery'
+import React, {Component} from 'react'
+
+import _ from 'underscore'
+
+console.log('javascript is working')
